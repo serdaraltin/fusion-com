aba3222dea5b0ee306146eb3226b9e03fa4eb799
serdaraltin <seyupaltin@gmail.com>
2024-11-03 22:08:09 +0300

    update network manager

---
+++ include/network/network_manager.hpp
@@ -0,0 +1,16 @@
+//
+// Created by main on 13.10.2024.
+//
+
+#ifndef FUSION_BRIDGE_NETWORK_MANAGER_HPP
+#define FUSION_BRIDGE_NETWORK_MANAGER_HPP
+
+namespace bridge {
+
+    class network_manager {
+
+    };
+
+} // bridge
+
+#endif //FUSION_BRIDGE_NETWORK_MANAGER_HPP
---
+++ src/network/network_manager.cpp
@@ -0,0 +1,8 @@
+//
+// Created by main on 13.10.2024.
+//
+
+#include "network/network_manager.hpp"
+
+namespace bridge {
+} // bridge
